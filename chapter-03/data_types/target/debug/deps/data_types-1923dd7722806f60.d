5612bbdacceeb71aa4b422f17f66e808f843210a
Felix Anducho <felix.anducho74@gmail.com>
2022-12-05 18:32:54 -0600

    Delete target folders

--- chapter-03/data_types/target/debug/deps/data_types-1923dd7722806f60.d
+++
@@ -1,5 +0,0 @@
-C:\Users\felix\OneDrive\Documentos\rust-programming-lang-book\chapter-03\data_types\target\debug\deps\data_types-1923dd7722806f60.exe: src\main.rs
-
-C:\Users\felix\OneDrive\Documentos\rust-programming-lang-book\chapter-03\data_types\target\debug\deps\data_types-1923dd7722806f60.d: src\main.rs
-
-src\main.rs:
--- chapter-03/data_types/target/debug/deps/data_types-1dcaee4c4ac742fe.d
+++
@@ -1,5 +0,0 @@
-c:\Users\felix\OneDrive\Documentos\rust-programming-lang-book\chapter-03\data_types\target\debug\deps\data_types-1dcaee4c4ac742fe.rmeta: src\main.rs
-
-c:\Users\felix\OneDrive\Documentos\rust-programming-lang-book\chapter-03\data_types\target\debug\deps\data_types-1dcaee4c4ac742fe.d: src\main.rs
-
-src\main.rs:
