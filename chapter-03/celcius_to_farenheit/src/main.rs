4b28fab3866ed8de45734e129fa638e815ed6f9d
Felix Anducho <felix.anducho74@gmail.com>
2022-12-06 18:10:50 -0600

    Add celcius to farenheit exercise from chapter 03

---
+++ chapter-03/celcius_to_farenheit/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::stdin;
+
+fn main() {
+    let mut user_input: String = String::new();
+    let stdin = stdin();
+
+    println!("Celcius to Farenheit convertor");
+    println!("Please input the amount to convert to farenheit: ");
+
+    stdin.read_line(&mut user_input)
+    .expect("Failed to read line");
+
+    let user_input: u32 = user_input.trim().parse().unwrap();
+
+    let celcius_from_fahrenheit = (user_input as f32 * 1.8000) + 32.00 as f32;
+
+    println!("The number of grades turned to Farenheit was: {}°F", celcius_from_fahrenheit);
+}
